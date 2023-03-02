34b0a82083fd12e0080881648cdfce80707cb1d7
Nikolaos Giannakis <nicko.fox155@gmail.com>
2023-03-02 22:53:52 +0100

    adding an data object for the navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
 import Footer from "./components/Footer";
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <About />
       <Services />
       <Tours />
       <Footer />
